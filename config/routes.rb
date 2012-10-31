6da55f614b83cabb7c61fd37de032e73876b5dc0
samantha <samantha.grimm@gmail.com>
2012-10-31 16:11:05 -0500

    Finished Layouts and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 
-
 TestApp::Application.routes.draw do
-
-
+  get "users/new"
+match '/signup',  :to => 'users#new' 
 match '/contact', :to => 'pages#contact'
-match '/about', :to => 'pages#about'
-match '/help', :to => 'pages#help'
-root :to => 'pages#home'
+match '/about',   :to => 'pages#about'
+match '/help',    :to => 'pages#help'
+root              :to => 'pages#home'
 
 
 
